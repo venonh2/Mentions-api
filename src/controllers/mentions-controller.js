a89c67e8ff77ba8a08df816a4f93bf4c19df8e63
venonh2 <brendow-evan@hotmail.com>
2019-10-22 13:48:51 -0300

    adição da pasta , repositories, começo da implementação do repository pattern, será movida as chamadas que são realizadas ao model-mentions

--- src/controllers/mentions-controller.js
+++ src/controllers/mentions-controller.js
@@ -5,8 +5,8 @@ const Mentions = mongoose.model('Mentions');
 //list
 exports.listMentions = async (req, res) => {
     try {
-        const data = await Mentions.find({});
-        res.status(200).send(data);
+        const data = await Mentions.find({}, `friend mention -_id`); // antes estava só find({}), ou seja traga tudo agor estamos dizendo traga só esses dois
+        res.status(200).send(data); // veja o '-_id' diz, cara não traga ele, serve para outras tbm
     } catch (e) {
         res.status(500).send({message: 'Falha ao carregar as menções' });
     }
